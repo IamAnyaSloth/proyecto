d5847fe9f2102646464b1984c62d96bf03516abe
IamAnyaSloth <anyebatallanosc@gmail.com>
2024-12-08 00:10:46 -0500

    Resueltos los conflictos

--- pages/contact.js
+++ pages/contact.js
@@ -3,6 +3,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ProfileCard from '../components/ProfileCard';
 import styles from '../styles/Contact.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faUser, faCommentDots } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => {
   return (
@@ -20,13 +22,19 @@ const Contact = () => {
       <div className={styles['contact-container']}>
         <h2>Contacto</h2>
         <form className={styles['contact-form']}>
-          <label>Nombre:
+          <label>
+            <FontAwesomeIcon icon={faUser} className={styles.icon} />
+            Nombre:
             <input type="text" placeholder="Tu nombre" />
           </label>
-          <label>Correo Electrónico:
+          <label>
+            <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+            Correo Electrónico:
             <input type="email" placeholder="Tu correo electrónico" />
           </label>
-          <label>Mensaje:
+          <label>
+            <FontAwesomeIcon icon={faCommentDots} className={styles.icon} />
+            Mensaje:
             <textarea placeholder="Escribe tu mensaje"></textarea>
           </label>
           <button type="submit">Enviar</button>
